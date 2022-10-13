78aee5b1658a3d393401874cf4e42e9b36a7e84b
Joseph Boscan <joeypc.py@gmail.com>
2022-10-12 23:46:44 -0400

    List cart UI ready

---
+++ components/cart/OrderSummary.tsx
@@ -0,0 +1,68 @@
+import NextLink from "next/link";
+import {
+  Box,
+  Button,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Link,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { initialData } from "../../database/products";
+import { ItemCounter } from "../ui";
+
+interface Props {}
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
+export const OrderSummary = (props: Props) => {
+  return (
+    <>
+      {productsInCart.map((product) => (
+        <Grid container spacing={2} key={product.slug} sx={{ mb: 1 }}>
+          <Grid item xs={3}>
+            <NextLink href="/product/slug" passHref>
+              <Link>
+                <CardActionArea>
+                  <CardMedia
+                    image={`products/${product.images[0]}`}
+                    component="img"
+                    sx={{ borderRadius: 5 }}
+                  />
+                </CardActionArea>
+              </Link>
+            </NextLink>
+          </Grid>
+
+          <Grid item xs={7}>
+            <Box display="flex" flexDirection="column">
+              <Typography variant="body1">{product.title}</Typography>
+              <Typography>
+                Talla: <strong>M</strong>
+              </Typography>
+              <ItemCounter />
+            </Box>
+          </Grid>
+
+          <Grid
+            item
+            xs={2}
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Typography variant="subtitle1">{`$${product.price}`}</Typography>
+            <Button variant="text" color="secondary">
+              Remover
+            </Button>
+          </Grid>
+        </Grid>
+      ))}
+    </>
+  );
+};
